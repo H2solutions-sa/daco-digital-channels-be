d56a1041af1cd06a930fdec2ce8ca4b29822e803
andrewfares <andrew@h2-solutions.co>
2025-10-29 01:25:03 +0300

    updating events

--- src/components/KFIA/Eastern-Province/EventsCards.tsx
+++ src/components/KFIA/Eastern-Province/EventsCards.tsx
@@ -25,7 +25,7 @@ type EventsCard = ComponentProps & {
 export const Default = (props: EventsCardsProps): JSX.Element => {
   const {t} = useI18n();
   const EventCards= props.fields.items && props.fields.items.map((e,index) =>
-        <article
+        <article key={index}
       className="
         bg-[#F9F9FB] rounded-[20px] shadow-sm
         w-full max-w-full lg:w-[574px]
@@ -91,21 +91,21 @@ export const Default = (props: EventsCardsProps): JSX.Element => {
   return (
     <div className='kfia-content'>
       <div className="max-w-[1200px] mx-auto mt-14 sm:mt-16">
-        <h2 className="text-[20px] sm:text-[22px] md:text-[28px] font-semibold text-[color:var(--kfia-brand)] text-center">
+        {/* <h2 className="text-[20px] sm:text-[22px] md:text-[28px] font-semibold text-[color:var(--kfia-brand)] text-center">
           {t("Events-title")}
         </h2>
         <p className="mt-3 text-center text-neutral-600 max-w-3xl mx-auto text-[14px] sm:text-[15px]">
           {t("events-subtitle")}
-        </p>
+        </p> */}
       </div>
 
       {/* LINKS & RESOURCES */}
-      <div className="max-w-[1200px] mx-auto mt-8 sm:mt-10">
-        <h3 className="text-[20px] sm:text-[22px] md:text-[28px] font-semibold text-[color:var(--kfia-brand)]">
+      <div className="max-w-[1200px] mx-auto mt-8 sm:mt-10 mb-4">
+        <h3 className="text-[20px] sm:text-[22px] md:text-[28px] font-semibold text-[color:var(--kfia-brand)] mt-8 sm:mt-10">
           {t("event-resources")}
         </h3>
       </div>
-      <div className="max-w-[1200px] mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 sm:gap-6 mt-4">
+      <div className="flex flex-col gap-5 w-full lg:w-[574px]">
         {EventCards}
       </div>
     </div>
